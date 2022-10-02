892df311a823b8637530d2a798a1079da75871ec
Ivan <vanesiotanaev@mail.ru>
2022-10-02 17:13:30 +0300

    seminar3 completed

--- seminar3.py
+++ seminar3.py
@@ -35,3 +35,38 @@
 # print(int(mod_user_value))
 # sum_value_nums = get_digits_sum(mod_user_value)
 # print(sum_value_nums)
+
+# Напишите программу, которая определит, присутствует ли в заданном списке строк искомое число.
+# Если присутствует, то в каком элементе?
+
+# def find_value(list, value):
+#     new_list = []
+#     for item in list:
+#         if str(value) in item:
+#             new_list.append(list.index(item))
+#     if len(new_list) > 0:
+#         return new_list
+#     else:
+#         print("Введенное значение отсутствует в списке")
+
+# user_list = ['abfg,', 'gg7ha', 'koa123', 'Gh700', 'fdgd']
+# user_num = int(input('Enter the number you want to find: '))
+# print(find_value(user_list, user_num))
+
+# Напишите программу, которая определит позицию второго вхождения
+# строки в список, либо сообщит, что ее нет:
+
+def find_2nd_entrance(list, text):
+    count = 0
+    for i in range(0, len(list)):
+        if str(text) in str(list[i]):
+            count += 1
+        if count == 2:
+            print(f'Второе вхождение строки "{text}" в список {list} находится по индексу {i}')
+            return i
+    else:
+        print("Второго вхождения введенного текста в список нет!")
+
+user_list = ['abc,', 123, 'asd', 'пщпщп,', 123, 'abc,', 'fло1', 'asd']
+user_text = (input('Enter the string you want to find: '))
+find_2nd_entrance(user_list, user_text)
